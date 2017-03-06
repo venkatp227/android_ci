6af4c8e7108f0c5f004c76d6f2bec4af84557fbf
MD AL MAMUN <shohag.csesust@gmail.com>
2017-03-06 00:18:19 -0500

    added deployment script

--- app/src/main/java/com/example/udemy/myapp01/MainActivity.java
+++ app/src/main/java/com/example/udemy/myapp01/MainActivity.java
@@ -3,11 +3,48 @@ package com.example.udemy.myapp01;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.UpdateManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkForUpdates();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // ... your own onResume implementation
+        checkForCrashes();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterManagers();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterManagers();
+    }
+
+    private void checkForCrashes() {
+        CrashManager.register(this);
+    }
+
+    private void checkForUpdates() {
+        // Remove this for store builds!
+        UpdateManager.register(this);
+    }
+
+    private void unregisterManagers() {
+        UpdateManager.unregister();
     }
 }
---
+++ scripts/deploy_hockeyapp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+############## ------------------ ############
+# This will have whole deployment code #######
+############## ------------------ ############
+
+# First paramter expected as APK path 
+APK_PATH=$1
+
+echo APK_PATH
+
+# Second parameter expected as access token
+ACCESS_TOKEN=$2
+
+echo ACCESS_TOKEN
+
+# Theird one is application id
+APP_ID=$3
+
+echo APP_ID
+
